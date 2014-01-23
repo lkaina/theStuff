94179f9bfd3c7ec6987f30470b9600e32219fd97
Leighton Kaina <leighton.kaina@gmail.com>
2014-01-23 14:01:00 -0800

    Change background, fix dropdown menu.

--- mySite.js
+++ mySite.js
@@ -1,24 +1,31 @@
-// $(document).ready(function(){
-//   $('.leftNav').hide();
-//   $('.leftHead nav').mouseover(function(event){
-//     $('.leftNav').stop(true, true).slideDown(300);
-//   });
-//   $('.leftHead nav').mouseleave(function(event){
-//     $('.leftNav').stop(true, true).slideUp(300);
-//   });
-//   $('main').hover(function(event){
-//     $(this).css({
-//       'transition-property': 'background-position',
-//       'transition-duration': '0.2s',
-//       'transition-timing-function': 'ease-out',
-//       'background-position': '1440px 0'
-//     });
-//   }, function(event){
-//     $(this).css({
-//       'transition-property': 'background-position',
-//       'transition-duration': '0.2s',
-//       'transition-timing-function': 'ease-out',
-//       'background-position': '0 0'
-//     });
-//   });
-// });
+$(document).ready(function(){
+  $('.leftNav').hide();
+  $('.leftHead nav').mouseover(function(event){
+    $('.leftNav').stop(true, true).slideDown(300);
+    $('.leftHead h1').css('color', 'rgb(0, 153, 255)');
+  });
+  $('.leftHead nav').mouseleave(function(event){
+    $('.leftNav').stop(true, true).slideUp(300);
+    $('.leftHead h1').css('color', 'black');
+  });
+  $('#bio').on('click', function(event){
+    event.preventDefault() && event.stopPropagation();
+    
+  });
+
+  // $('main').hover(function(event){
+  //   $(this).css({
+  //     'transition-property': 'background-position',
+  //     'transition-duration': '0.2s',
+  //     'transition-timing-function': 'ease-out',
+  //     'background-position': '1440px 0'
+  //   });
+  // }, function(event){
+  //   $(this).css({
+  //     'transition-property': 'background-position',
+  //     'transition-duration': '0.2s',
+  //     'transition-timing-function': 'ease-out',
+  //     'background-position': '0 0'
+  //   });
+  // });
+});
