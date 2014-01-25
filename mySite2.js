382551a5e472bb3375b7c8db799c82690dbcab07
Leighton Kaina <leighton.kaina@gmail.com>
2014-01-24 20:57:19 -0800

    Remove old files.

--- mySite2.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('.leftNav ul').hide();
-  $('header h1').mouseover(function(event){
-    $('.leftNav ul').slideDown(300);
-  });
-  $('.leftNav').mouseleave(function(event){
-    $('.leftNav ul').slideUp(300);
-  })
-});
